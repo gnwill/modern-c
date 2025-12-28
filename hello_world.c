99e5a708a40bdc32ad69c9e48f8b15c549417606
William Gonzalez <gonzalez.williamalexander1@gmail.com>
2025-12-27 19:28:36 -0500

    Small C programs

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+// Simple hello world program written in C.
+// Author: William Gonzalez <email: will@wgz.sh>
+# include <stdio.h>
+
+int main(void) {
+    
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ pun.c
@@ -0,0 +1,10 @@
+// Simple pun program written in C.
+// Author: William Gonzalez <email: will@wgz.sh>
+# include <stdio.h>
+
+int main(void) {
+    
+    printf("To C, or not to C: that is the question.\n");
+
+    return 0;
+}
