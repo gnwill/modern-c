3bc7afc5a92925b4730a5f2a19163fc90ff761ca
William Gonzalez <gonzalez.williamalexander1@gmail.com>
2025-12-28 22:16:09 -0500

    Simple print program

---
+++ Chapter 3/simple_print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    float y = 40.0f;
+    float z = 50.0f;
+
+    printf("a = %d, b = %d , c = %d , y = %.2f, z = %.2f\n", a,b,c,y,z);
+
+    return 0;
+    
+
+}
