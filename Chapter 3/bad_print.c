81f45c0ceab4c1c27289fface55d570f894fbf20
William Gonzalez <gonzalez.williamalexander1@gmail.com>
2025-12-29 22:14:09 -0500

    This is a bad program

---
+++ Chapter 3/bad_print.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    float y = 40.0f;
+    float z = 50.0f;
+
+    printf("a = %d, b = %d , c = %d , y = %.2f, z = %.2f\n", a);
+    printf("x = %f", a);
+
+
+    return 0;
+    
+}
